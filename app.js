660d6fac3d4af0cf11ed96c9e96415ed779b0a70
searchingeXly <searchingeXly@gmail.com>
2021-11-30 11:12:12 -0500

    7 - modal

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const toggleBtn = document.querySelector(".sidebar-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const sidebar = document.querySelector(".sidebar");
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
 
-toggleBtn.addEventListener("click", function () {
-  sidebar.classList.toggle("show-sidebar");
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+  modalOverlay.classList.add("open-modal");
 });
 
 closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+  modalOverlay.classList.remove("open-modal");
 });
