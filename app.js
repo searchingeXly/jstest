0f99a64f38c9bb9270a4750ad07af4c39e0d61ef
searchingeXly <searchingeXly@gmail.com>
2021-11-30 17:12:59 -0500

    working on giveaway countdown

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-const btns = document.querySelectorAll(".tab-btn");
-const about = document.querySelector(".about");
-const articles = document.querySelectorAll(".content");
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-about.addEventListener("click", function (e) {
-  const id = e.target.dataset.id;
-  if (id) {
-    // remove active from other buttons
-    btns.forEach(function (btn) {
-      btn.classList.remove("active");
-      e.target.classList.add("active");
-    });
-    // hide other articles
-    articles.forEach(function (article) {
-      article.classList.remove("active");
-      if (article.id === id) {
-        article.classList.add("active");
-      }
-    });
-  }
-});
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+console.log(items);
+
+let futureDate = new Date(2021, 11, 31, 23, 59, 59, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+giveaway.textContent = `giveaway ends on ${year} ${hours}:${mins}`;
