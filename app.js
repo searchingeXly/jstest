e1ef5940078f01fa5d554bc8f3c336777c793505
searchingeXly <searchingeXly@gmail.com>
2021-11-30 15:43:05 -0500

    11 - scroll

--- app.js
+++ app.js
@@ -1,23 +1,72 @@
-// MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
 
-const btn = document.querySelector(".switch-btn");
-const video = document.querySelector(".video-container");
+// ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
-btn.addEventListener("click", function () {
-  if (!btn.classList.contains("slide")) {
-    btn.classList.add("slide");
-    video.pause();
+navToggle.addEventListener("click", function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
   } else {
-    btn.classList.remove("slide");
-    video.play();
+    linksContainer.style.height = 0;
   }
 });
+// ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 
-// preloader
-const preloader = document.querySelector(".preloader");
+window.addEventListener("scroll", function (e) {
+  const scrollHeight = window.scrollY;
+  const navHeight = navbar.getBoundingClientRect().height;
 
-window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader");
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
 });
