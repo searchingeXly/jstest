da32bf3eac59f5da282f10e47dc89aae9de866d0
searchingeXly <searchingeXly@gmail.com>
2021-11-29 23:49:51 -0500

    3 - counter

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// set initial count
+let count = 0;
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-btn.addEventListener("click", function () {
-  // get random number between 0 and 3
-  const randomNumber = getRandomNumber();
-
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else if (styles.contains("reset")) {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
 });
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
-}
