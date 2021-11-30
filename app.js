e26a2f592550046c4aa664887551083b56ef0cae
searchingeXly <searchingeXly@gmail.com>
2021-11-30 11:41:59 -0500

    8 - questions

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+//using selectors inside the element
 
-const modalBtn = document.querySelector(".modal-btn");
-const modalOverlay = document.querySelector(".modal-overlay");
-const closeBtn = document.querySelector(".close-btn");
+const questions = document.querySelectorAll(".question");
 
-modalBtn.addEventListener("click", function () {
-  modalOverlay.classList.add("open-modal");
+questions.forEach(function (question) {
+  //   console.log(question);
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
 });
 
-closeBtn.addEventListener("click", function () {
-  modalOverlay.classList.remove("open-modal");
-});
+// traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
