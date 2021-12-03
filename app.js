494631666f8c0e88e5d35b4543a31b0cee10e92a
searchingeXly <searchingeXly@gmail.com>
2021-12-02 21:48:30 -0500

    23 - stripe submenus

--- app.js
+++ app.js
@@ -41,7 +41,44 @@ linkBtns.forEach((btn) => {
     // console.log(e.currentTarget);
     const text = e.currentTarget.textContent;
     const tempBtn = e.currentTarget.getBoundingClientRect();
-    console.log(text, tempBtn);
-    submenu.classList.add("show");
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    const tempPage = sublinks.find(({ page }) => page === text);
+    if (tempPage) {
+      const { page, links } = tempPage;
+      submenu.classList.add("show");
+      submenu.style.left = `${center}px`;
+      submenu.style.top = `${bottom}px`;
+      let columns = "col-2";
+      if (links.length === 3) {
+        columns = "col-3";
+      }
+      if (links.length >= 4) {
+        columns = "col-4";
+      }
+      submenu.innerHTML = `
+      <section>
+      <h4>${page}</h4>
+      <div class="submenu-center ${columns}">
+      ${links
+        .map((link) => {
+          return `<a href="${link.url}">
+        <i class="${link.icon}"></i> ${link.label}
+        </a>`;
+        })
+        .join("")}
+      </div>
+      </section>`;
+    }
   });
+});
+
+hero.addEventListener("mouseover", function (e) {
+  submenu.classList.remove("show");
+});
+
+nav.addEventListener("mouseover", function (e) {
+  if (!e.target.classList.contains("link-btn")) {
+    submenu.classList.remove("show");
+  }
 });
