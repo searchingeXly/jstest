1f6224696424cc63a8b898b4a17af7d7e822468d
searchingeXly <searchingeXly@gmail.com>
2021-12-03 10:37:50 -0500

    Worked on products page with filters

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,6 +1,30 @@
-import { getElement } from '../utils.js';
-import display from '../displayProducts.js';
+import { getElement } from "../utils.js";
+import display from "../displayProducts.js";
 
-const setupCompanies = () => {};
+const setupCompanies = (store) => {
+  let companies = ["all", ...new Set(store.map((product) => product.company))];
+
+  const companiesDOM = getElement(".companies");
+  companiesDOM.innerHTML = companies
+    .map((company) => {
+      return `<button class="company-btn">${company}</button>`;
+    })
+    .join("");
+  companiesDOM.addEventListener("click", function (e) {
+    const element = e.target;
+    if (element.classList.contains("company-btn")) {
+      let filteredStore = [];
+      if (element.textContent.toLowerCase() === "all") {
+        filteredStore = [...store];
+      } else {
+        filteredStore = store.filter(
+          (product) =>
+            product.company.toLowerCase() === element.textContent.toLowerCase()
+        );
+      }
+      display(filteredStore, getElement(".products-container"));
+    }
+  });
+};
 
 export default setupCompanies;
