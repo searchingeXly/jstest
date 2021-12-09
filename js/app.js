ccf5da396c09c0212964df73fc5be6907e025e6e
searchingeXly <searchingeXly@gmail.com>
2021-12-09 15:42:13 -0500

    HTML/CSS - 2 - Backroads Tours

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 // ********** set date ************
 // select span
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 
 // ********** nav toggle ************
 // select button and links
@@ -16,8 +15,8 @@ navBtn.addEventListener("click", () => {
 // ********** smooth scroll ************
 // select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
     // prevent default
     e.preventDefault();
     links.classList.remove("show-links");
@@ -26,12 +25,11 @@ scrollLinks.forEach(link => {
     const element = document.getElementById(id);
     //
     let position = element.offsetTop - 62;
-
     window.scrollTo({
       left: 0,
       // top: element.offsetTop,
       top: position,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   });
 });
